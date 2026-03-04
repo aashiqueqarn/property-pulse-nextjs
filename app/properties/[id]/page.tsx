3d76a88ee9b3d4b2b81942d0a036e748c4a3db8d
Aashique Karn <aashiquekarn@gmail.com>
2026-03-04 23:45:01 +0530

    master | adding necessary codes

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,9 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
 
 export default async function PropertyPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
@@ -9,7 +12,23 @@ export default async function PropertyPage({ params }: { params: Promise<{ id: s
     return (
         <>
             <PropertyHeaderImage image={property?.images[0]} />
-            <section>{property?.name}</section>
+            <section>
+                <div className='container m-auto py-6 px-6'>
+                    <Link
+                        href='/properties'
+                        className='text-blue-500 hover:text-blue-600 flex items-center'
+                    >
+                        <FaArrowLeft className='mr-2' /> Back to Properties
+                    </Link>
+                </div>
+            </section>
+            <section className="bg-blue-50">
+                <div className="container m-auto py-10 px-6">
+                    <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                        <PropertyDetails property={property} />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,16 @@
 export default function Hero() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">Hero Component</h1>
-        </div>
+        <section className='bg-blue-700 py-20 mb-4'>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
+                <div className='text-center'>
+                    <h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>
+                        Find The Perfect Rental
+                    </h1>
+                    <p className='my-4 text-xl text-white'>
+                        Discover the perfect property that suits your needs.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 }
